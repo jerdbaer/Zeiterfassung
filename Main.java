6f9bf71e272c7af60278f3e2dbe3380b7b453ef5
jerdbaer <de_kleene_josi@yahoo.de>
2021-05-28 11:54:48 +0200

    init

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("HI");
+        System.out.println("");
+    }
+    
+}
+//git config --global user.email "you@example.com"
+  
