31e697a683b68012779d7e2eee5aa0e68c591fc6
TomxHWRxWI20C <s_weissflog20@stud.hwr-berlin.de>
2021-05-31 14:32:24 +0200

    function name change

--- Breaktime_calculation.java
+++ Breaktime_calculation.java
@@ -1,4 +1,25 @@
 
-public class Breaktime_calculation {
+public class Breaktime_calculation 
+{
+	/*
+	 *  < 6h keine Pause
+	 *  6-9h 30min 
+	 *  > 9h 45min
+	 *  
+	 *  
+	 *  _Pause_
+	 *  min. 15min am stück
+	 *  nicht am Anfang, am Ende der Arbeit
+	 */
 
+	public double getBreaktime_min_in_h(double worktime_in_h)
+	{
+		double breaktime_min_in_h = 0;
+		if(worktime_in_h > 6)
+			breaktime_min_in_h += 0.5;
+		if (worktime_in_h > 9)
+			breaktime_min_in_h += 0.25;
+		return breaktime_min_in_h;
+			
+	}
 }
