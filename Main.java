2f39ce1a7fbae031f2456b74ab8a9dea3deb2c5d
valientes <simon.valiente@gmx.de>
2021-06-02 13:39:20 +0200

    TRying to resolve the merge conflict with the dev branch

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 
 public class Main {
     public static void main(String[] args){
-        
-    }
-    
-}
+        System.out.println("HI");
+        System.out.println("");
+        System.out.println("geändert");
+        System.out.println("x");
 
-  
+        System.out.println("xxxx");
+        System.out.println("great!");
+        System.out.println("Ticket #31");
+    }
+
+}
+//git config --global user.email "you@example.com"
