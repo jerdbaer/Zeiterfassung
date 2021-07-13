745416d66cc2ce93ad019f3cd77b4ba02818d00c
valientes <simon.valiente@gmx.de>
2021-07-13 21:08:57 +0200

    WIP: Connection to Database

---
+++ database/InitDB.java
@@ -0,0 +1,114 @@
+package database;
+
+import java.sql.*;
+
+public class InitDB{
+
+  private Connection connection;
+
+  static {
+    try {
+      Class<?> c = Class.forName("com.mysql.jdbc.Driver");
+      if (c != null){
+        System.out.println("JDBC-Treiber geladen");
+      }
+    } catch (ClassNotFoundException e){
+      System.err.println(e);
+      System.exit(1);
+    }
+  }
+
+  public InitDB() {
+    createConnection();
+    createDBStructure();
+    Thread shutDownHook = new Thread() {
+      public void run() {
+        System.out.println("Running shutdown hook");
+        if(connection == null)
+          System.out.println("Connection to database already closed");
+        try {
+          if (connection != null && !connection.isClosed()){
+            connection.close();
+            if (connection.isClosed())
+              System.out.println("Connection to database closed");
+          }
+        } catch (SQLException e){
+          System.err.println("Shutdown hook couldn't close database connection.");
+        }
+      }
+    };
+    Runtime.getRuntime().addShutdownHook(shutDownHook);
+  }
+
+  private void createConnection() {
+    String url = "jdbc:mysql://localhost/?rewriteBatchedStatements=true";
+    String user = "root";
+    String pass = "";
+    try {
+      System.out.println("Creating DBConnection");
+      connection = DriverManager.getConnection(url, user, pass);
+    } catch (SQLException e) {
+      System.err.println("Couldn't create DBConnection");
+      System.exit(1);
+    }
+  }
+
+  private boolean createDBStructure() {
+    String dbName = "Zeiterfassung";
+    String query0 = "CREATE DATABASE IF NOT EXISTS `" + dbName + "`";
+    String query1 = "USE `" + dbName + "`";
+    String query2 = "SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO'; ";
+    String table0 = "CREATE TABLE IF NOT EXISTS `Mitarbeiter` ("
+            + "`MA_ID` int NOT NULL, "
+            + "`MA_Vorname` char(50) NOT NULL, "
+            + "`MA_Nachname` char(50) NOT NULL, "
+            + "`MA_Strasse` char(50) NOT NULL, "
+            + "`MA_PLZ` char(5) NOT NULL, "
+            + "`MA_Ort` char(50) NOT NULL, "
+            + "`MA_Abteilung` char(50) NOT NULL, "
+            + "`MA_E-Mail` char(50) NOT NULL, "
+            + "`MA_Urlaubstage` int NOT NULL, "
+            + "`MA_aktiv` enum('ja', 'nein') NOT NULL, "
+            + "PRIMARY KEY (MA_ID));";
+    String table1 = "CREATE TABLE IF NOT EXISTS `Zeitkonto` ("
+            + "`work_date` date NOT NULL, "
+            + "`MA_ID` int NOT NULL, "
+            + "`Arbeitszeit_Ende` time NOT NULL, "
+            + "`Arbeitszeit_Beginn` time NOT NULL, "
+            + "`Ueberstunden_Tag` time NOT NULL, "
+            + "CONSTRAINT `PK_Zeitkonto` PRIMARY KEY (work_date,MA_ID), "
+            + "FOREIGN KEY (MA_ID) REFERENCES Mitarbeiter(MA_ID));";
+
+    Statement stmt = null;
+    try{
+      connection.setAutoCommit(false);
+      stmt = connection.createStatement();
+      stmt.addBatch(query0);
+      stmt.addBatch(query1);
+      stmt.addBatch(query2);
+      stmt.addBatch(table0);
+      stmt.addBatch(table1);
+      stmt.executeBatch();
+      connection.commit();
+      stmt.close();
+      connection.close();
+      System.out.println("Database successfully created or just existing");
+      return true;
+    } catch (SQLException e){
+      e.printStackTrace();
+    } finally {
+      try{
+        if (stmt != null)
+          stmt.close();
+        if (connection != null)
+          connection.close();
+      } catch (SQLException e){
+      }
+    }
+    return false;
+  }
+
+  public static void main(String[] args){
+    new InitDB();
+  }
+}
