6ad6ce5bebad3be6fe4d7cda0e281ff8da214cd3
TomxHWRxWI20C <s_weissflog20@stud.hwr-berlin.de>
2021-07-14 21:04:36 +0200

    added Textformatter to restrict input (time input only 00:00 - 23:59 posible)

--- controller/MainController.java
+++ controller/MainController.java
@@ -1,12 +1,28 @@
 package controller;
 
+import java.util.function.UnaryOperator;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 import javafx.scene.layout.HBox;
 
 public class MainController {
 
+    @FXML
+    private TextField txtfieldWorkStart1Hours;
+
+    @FXML
+    private TextField txtfieldWorkStart1Minutes;
+
+    @FXML
+    private TextField txtfieldWorkEnd1Hours;
+
+    @FXML
+    private TextField txtfieldWorkEnd1Minutes;
 
     @FXML
     private Button btnWorkAdd1;
@@ -15,10 +31,46 @@ public class MainController {
     private HBox hBoxWork2;
 
     @FXML
+    private TextField txtfieldWorkStart2Hours;
+
+    @FXML
+    private TextField txtfieldWorkStart2Minutes;
+
+    @FXML
+    private TextField txtfieldWorkEnd2Hours;
+
+    @FXML
+    private TextField txtfieldWorkEnd2Minutes;
+
+    @FXML
     private Button btnWorkAdd2;
 
     @FXML
     private HBox hBoxWork3;
+
+    @FXML
+    private TextField txtfieldWorkStart3Hours;
+
+    @FXML
+    private TextField txtfieldWorkStart3Minutes;
+
+    @FXML
+    private TextField txtfieldWorkEnd3Hours;
+
+    @FXML
+    private TextField txtfieldWorkEnd3Minutes;
+
+    @FXML
+    private TextField txtfieldBreakStart1Hours;
+
+    @FXML
+    private TextField txtfieldBreakStart1Minutes;
+
+    @FXML
+    private TextField txtfieldBreakEnd1Hours;
+
+    @FXML
+    private TextField txtfieldBreakEnd1Minutes;
 
     @FXML
     private Button btnBreakAdd1;
@@ -27,10 +79,34 @@ public class MainController {
     private HBox hBoxBreak2;
 
     @FXML
+    private TextField txtfieldBreakStart2Hours;
+
+    @FXML
+    private TextField txtfieldBreakStart2Minutes;
+
+    @FXML
+    private TextField txtfieldBreakEnd2Hours;
+
+    @FXML
+    private TextField txtfieldBreakEnd2Minutes;
+
+    @FXML
     private Button btnBreakAdd2;
 
     @FXML
     private HBox hBoxBreak3;
+
+    @FXML
+    private TextField txtfieldBreakStart3Hours;
+
+    @FXML
+    private TextField txtfieldBreakStart3Minutes;
+
+    @FXML
+    private TextField txtfieldBreakEnd3Hours;
+
+    @FXML
+    private TextField txtfieldBreakEnd3Minutes;
 
     @FXML
     private Button btnBreakAdd3;
@@ -39,17 +115,111 @@ public class MainController {
     private HBox hBoxBreak4;
 
     @FXML
+    private TextField txtfieldBreakStart4Hours;
+
+    @FXML
+    private TextField txtfieldBreakStart4Minutes;
+
+    @FXML
+    private TextField txtfieldBreakEnd4Hours;
+
+    @FXML
+    private TextField txtfieldBreakEnd4Minutes;
+
+    @FXML
     private Button btnBreakAdd4;
 
     @FXML
     private HBox hBoxBreak5;
 
     @FXML
-    private Button btnBreakAdd5;
+    private TextField txtfieldBreakStart5Hours;
+
+    @FXML
+    private TextField txtfieldBreakStart5Minutes;
+
+    @FXML
+    private TextField txtfieldBreakEnd5Hours;
+
+    @FXML
+    private TextField txtfieldBreakEnd5Minutes;
+
+
+	    
+    @FXML
+    public void initialize() {
+    	
+    	//restrict Input
+    	txtfieldWorkStart1Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldWorkStart2Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldWorkStart3Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	
+    	txtfieldWorkEnd1Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldWorkEnd2Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldWorkEnd3Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	
+    	txtfieldBreakStart1Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakStart2Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakStart3Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakStart4Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakStart5Hours.setTextFormatter(new TextFormatter<String>(hours));
+
+    	txtfieldBreakEnd1Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakEnd2Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakEnd3Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakEnd4Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	txtfieldBreakEnd5Hours.setTextFormatter(new TextFormatter<String>(hours));
+    	
+    	
+    	txtfieldWorkStart1Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldWorkStart2Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldWorkStart3Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	
+    	txtfieldWorkEnd1Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldWorkEnd2Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldWorkEnd3Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	
+    	txtfieldBreakStart1Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakStart2Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakStart3Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakStart4Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakStart5Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+
+    	
+    	txtfieldBreakEnd1Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakEnd2Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakEnd3Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakEnd4Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+    	txtfieldBreakEnd5Minutes.setTextFormatter(new TextFormatter<String>(minutes));
+
+    	
+    }
+    
+    //restriction rules for hours
+    private UnaryOperator<Change> hours = change -> 
+    {
+	    if (!change.isContentChange()) 
+	    	return change;
+	    
+	    String text = change.getControlNewText();
+	    
+	    return text.matches("^([0-9]|0[0-9]|1[0-9]|2[0-3])$") || text.equals("") ? change : null ;
+	};
+	
+	//restriction rules for minutes
+    private UnaryOperator<Change> minutes = change -> 
+    {
+	    if (!change.isContentChange()) 
+	    	return change;
+	    
+	    String text = change.getControlNewText();
+	    
+	    return text.matches("^([0-9]|[0-5][0-9])$") || text.equals("") ? change : null ;
+	};
 
     @FXML
     void showHBox(ActionEvent event) 
-    {
+    {   	
     	Button buttonpressed = (Button) event.getSource();
     	if(buttonpressed == btnWorkAdd1)
     	{
@@ -83,5 +253,7 @@ public class MainController {
     	}
 
     }
+    
+    
 
 }
