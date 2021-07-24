2cddbde9e9581409c3e678d5d1c34a0abb561b86
TomxHWRxWI20C <s_weissflog20@stud.hwr-berlin.de>
2021-07-24 22:56:55 +0200

    init MA_Data to sove Ma_ID + Password

---
+++ models/MA_Data.java
@@ -0,0 +1,25 @@
+package models;
+
+public class MA_Data {
+	
+	private int MA_ID;
+	private String password;
+	
+	
+	public int getMA_ID() {
+		return MA_ID;
+	}
+	
+	public void setMA_ID(int MA_ID) {
+		this.MA_ID = MA_ID;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String codedPassword) {
+		this.password = codedPassword;
+	}
+
+}
