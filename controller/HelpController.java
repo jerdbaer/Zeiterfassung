c034780ee2f96f4140918c977340230157237899
TomxHWRxWI20C <s_weissflog20@stud.hwr-berlin.de>
2021-07-24 20:26:18 +0200

    init Help controller and edit Menu (enabled Button goto Help)

--- controller/HelpController.java
+++ controller/HelpController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class MenuController {
+public class HelpController {
 
     @FXML
     private Button btnWorktimeTop;
@@ -20,33 +20,21 @@ public class MenuController {
 
     @FXML
     private Button btnLogout;
-
-    @FXML
-    private Button btnWorktime;
-
-    @FXML
-    private Button btnAbsence;
-
-    @FXML
-    private Button btnOverview;
-
-    @FXML
-    private Button btnHelp;
     
     @FXML
     void switchScene(ActionEvent event) {
     	var swapStageController = new SwapStageController();
     	var pressedBtn = (Button)event.getSource();
-    	if(pressedBtn == btnWorktimeTop || pressedBtn == btnWorktime) {
+    	if(pressedBtn == btnWorktimeTop) {
     		swapStageController.goTo("/view/WorkTime.fxml");
     	}
-    	else if(pressedBtn == btnAbsenceTop || pressedBtn == btnAbsence) {
+    	else if(pressedBtn == btnAbsenceTop) {
  //   		swapStageController.goToAbsence(event);
     	}
-    	else if(pressedBtn == btnOverviewTop || pressedBtn == btnOverview) {
+    	else if(pressedBtn == btnOverviewTop) {
 //    		swapStageController.goToOverview(event);
     	}
-    	else if(pressedBtn == btnHelpTop || pressedBtn == btnHelp) {
+    	else if(pressedBtn == btnHelpTop) {
     		swapStageController.goTo("/view/Help.fxml");
     	}
     	else if(pressedBtn == btnLogout) {
@@ -54,8 +42,4 @@ public class MenuController {
     	}
 
     }
-
-
-
-    	
-}
+}  
