6694a74e91c64fabf838418d8747c2d47836adec
TomxHWRxWI20C <s_weissflog20@stud.hwr-berlin.de>
2021-07-25 03:29:27 +0200

    added sample data to the Chart

--- controller/OverviewController.java
+++ controller/OverviewController.java
@@ -2,6 +2,8 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
 public class OverviewController {
@@ -33,6 +35,9 @@ public class OverviewController {
 
     @FXML
     private Button btnWorkTime;
+    
+    @FXML
+    private LineChart<Number, Number> chart;
 
     @FXML
     void switchScene(ActionEvent event) {
@@ -58,7 +63,27 @@ public class OverviewController {
     
     @FXML
     void showSampleData(ActionEvent event) {
+    	chart.getData().clear();
+    	chart.setCreateSymbols(false);
     	
-    }
+    	final int MINIMUM = 6;
+    	final int MAXIMUM = 10;
+    	final int SOLL = 8;
+    	var workTime = new XYChart.Series<Number, Number>();
+    	workTime.setName("Arbeitszeit");
+    	var sollTime = new XYChart.Series<Number, Number>();
+    	sollTime.setName("Soll Arbeitszeit");
+  
+		for (int i=0;i<=31;i++)
+		{
+			var randomNum = MINIMUM + (int)(Math.random() * (MAXIMUM - MINIMUM));
+			sollTime.getData().add(new XYChart.Data<Number, Number>(i,SOLL));
+			workTime.getData().add(new XYChart.Data<Number, Number>(i,randomNum));
+		}
+		chart.getData().add(sollTime);					
+		chart.getData().add(workTime);
+		sollTime.nodeProperty().get().setStyle("-fx-stroke: #0f358e;");
+		workTime.nodeProperty().get().setStyle("-fx-stroke: #f92047;");
+	}    
 
 }
